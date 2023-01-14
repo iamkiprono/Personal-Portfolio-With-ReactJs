23f75e69c1a39ca354befb8aead8ade9d665bdfb
iamkiprono <101986591+iamkiprono@users.noreply.github.com>
2023-01-14 11:15:08 +0300

    Updates

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,24 +1,40 @@
-import React from 'react'
-import { useEffect } from 'react'
-import { useState } from 'react'
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import "./Footer.css";
 
 const Footer = () => {
-
-    const [copyRight, setCopyRight] = useState("2000")
-    const update = () => {
-
-        const dateNow = new Date().getFullYear()
-        setCopyRight(dateNow)
-    }
-    useEffect(update)
+  const [copyRight, setCopyRight] = useState("2000");
+  const update = () => {
+    const dateNow = new Date().getFullYear();
+    setCopyRight(dateNow);
+  };
+  useEffect(update);
 
   return (
-    <div>
-       
-        {copyRight}
-
+    <div className="footer">
+      <div className="social-links">
+        <ul>
+          <li>
+            <a href="https://github.com/iamkiprono" target="_blank">
+              <i class="fa-brands fa-github"></i>
+            </a>
+          </li>
+          <li>
+            <a href="https://github.com/iamkiprono" target="_blank">
+              <i class="fa-brands fa-linkedin"></i>
+            </a>
+          </li>
+          <li>
+            <a href="https://github.com/iamkiprono" target="_blank">
+              <i class="fa-brands fa-whatsapp"></i>
+            </a>
+          </li>
+        </ul>
+      </div>
+      <p>{copyRight}</p>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -4,6 +4,8 @@ import htmlicon from "../assets/html.png";
 import cssicon from "../assets/css.png";
 import jsicon from "../assets/js.png";
 import reacticon from "../assets/react.png";
+import githubicon from "../assets/github.png";
+
 
 const Skills = () => {
   return (
@@ -33,19 +35,7 @@ const Skills = () => {
         <div className="skills-cards">
           <div className="html-card card">
             <p>Github</p>
-            <img src={htmlicon} alt="" />
-          </div>
-          <div className="CSS-card card">
-            <p>CSS</p>
-            <img src={cssicon} alt="" />
-          </div>
-          <div className="JavaScript-card card">
-            <p>JavaScript</p>
-            <img src={jsicon} alt="" />
-          </div>
-          <div className="React-card card">
-            <p>React</p>
-            <img src={reacticon} alt="" />
+            <img src={githubicon} alt="" />
           </div>
         </div>
       </div>
