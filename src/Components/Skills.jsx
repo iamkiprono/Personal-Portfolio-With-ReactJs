2ad11a2977b7fa13f587b34ce317ff308ee40258
iamkiprono <101986591+iamkiprono@users.noreply.github.com>
2023-01-15 10:56:47 +0300

    Update Skills.jsx

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -9,7 +9,10 @@ import githubicon from "../assets/github.png";
 
 const Skills = () => {
   return (
+    
     <div className="skills">
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1228355685509098"
+     crossorigin="anonymous"></script>
       <div className="skills-container">
         <h3>Skills</h3>
         <p>Front-End Skills</p>
