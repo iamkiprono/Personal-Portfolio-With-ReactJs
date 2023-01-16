c5cc0aa993c9573256c9a198c8fbc1d2d638c1e7
iamkiprono <101986591+iamkiprono@users.noreply.github.com>
2023-01-16 17:30:03 +0300

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
                 <li><a href="#skills">Skills</a></li>
                 <li><a href="#portfolio">Portfolio</a></li>
                 <li><a href="#contact">Contact</a></li>
-                <li><a className='resume' href="kipronoresume.netlify.app" target="_blank">View Resume</a></li>
+                <li><a className='resume' href="https://kipronoresume.netlify.app" target="_blank">View Resume</a></li>
             </ul>
         </div>
 
