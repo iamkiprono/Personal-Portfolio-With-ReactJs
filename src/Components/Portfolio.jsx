6da9eea8be6d07ecd278429a2b63f2a84dd05981
Collins Kiprono Kirui <101986591+iamkiprono@users.noreply.github.com>
2023-03-18 11:26:32 +0300

    Updates

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -1,25 +1,47 @@
 import React from "react";
 import "./Portfolio.css";
 import fulltime from "../assets/360.jpg";
+import countries from "../assets/countries.JPG";
 
 const Portfolio = () => {
   return (
     <div id="portfolio" className="portfolio">
-      <h3>Projects</h3>
-      <div className="projects">
-        <div className="portfolio-container">
-          <img src={fulltime} alt="" />
-          <div className="project-details">
-            <h3>FullTime360</h3>
-            
+      <div className="portfolio-container">
+        <h3 className="project-heading">Projects</h3>
+        <div className="card-holder">
+          <div className="card-portfolio">
+            <img src={fulltime} alt="" />
+            <h4>Name: FullTime360</h4>
             <p>
-              This is a Football News website that fetches news from an external
-              JSON file and renders to the page
+              This is a football news website that consumes a RestAPI for the
+              news updates. The user can sign in an make changes on the databse
+              and the website will automatically fetch the updated news using
+              the fetch API and render on the webpage
             </p>
-
-            <div className="project-links">
-              <a href="https://fulltime360.netlify.app/" target="_blank">Visit</a>
-              <a href="https://github.com/iamkiprono/FullTime-360-Website" target="_blank">View on Github</a>
+            <h4>Tech:</h4>
+            <p>Vanilla JavaScript</p>
+            <div className="btns">
+              <a href="">View live</a>
+              <a href="">View on Github</a>
+            </div>
+          </div>
+          <div className="card-portfolio">
+            <img src={countries} alt="" />
+            <h4>Name: Rest API Frontend Mentor Challenge</h4>
+            <p>
+              This is a Front End Mentor challenge that requires to build an
+              exact replica of a design and make use of an API to fetch the
+              countries' data and display on the webpage. It also has search
+              capability and the user can filter through countries using the
+              search bar
+            </p>
+            <h4>Tech:</h4>
+            <p>React</p>
+            <div className="btns">
+              <a target="_blank" href="https://countriescollins.netlify.app/">
+                View live
+              </a>
+              <a href="">View on Github</a>
             </div>
           </div>
         </div>
