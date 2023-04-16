6c0afc42dd1e62668907c0f691198e7eb702a31e
Collins Kiprono Kirui <101986591+iamkiprono@users.noreply.github.com>
2023-04-16 15:10:36 +0300

    Updates

--- src/assets/Components/Skills.jsx
+++ src/assets/Components/Skills.jsx
@@ -2,11 +2,13 @@ import React, { useEffect, useState } from "react";
 
 const Skills = () => {
   const [skills, setSkills] = useState([]);
+  const [loading, setLoading] = useState(false);
   const getSkills = async () => {
+    setLoading(true);
     const res = await fetch("/skills.json");
     const data = await res.json();
+    setLoading(false);
     setSkills(data);
-    console.log(data);
   };
   useEffect(() => {
     getSkills();
@@ -16,14 +18,16 @@ const Skills = () => {
       <div className="container">
         <h3 className="headings">Skills</h3>
         <div className="skills-cards">
-          {skills.map((skill) => {
-            return (
-              <div className="card">
-                <img src={skill.icon} alt="" />
-                <h3>{skill.skill}</h3>
-              </div>
-            );
-          })}
+          {loading
+            ? "Loading..."
+            : skills.map((skill) => {
+                return (
+                  <div className="card">
+                    <img src={skill.icon} alt="" />
+                    <h3>{skill.skill}</h3>
+                  </div>
+                );
+              })}
         </div>
       </div>
     </div>
